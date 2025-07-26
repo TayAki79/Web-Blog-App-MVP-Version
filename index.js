8610e8a240e2afdd10bdc225971509706db0b9f2
Akradeth Tayarat <a.tayarat@gmx.net>
2025-07-26 22:12:25 +0200

    Implementing body-parser, adding a post-request, writing a form in index.ejs

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 import express from "express";
+import bodyParser from "body-parser";
 
+const posts = [];
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", { posts });
 });
+
+app.post("/posts", (req, res)=>{
+  const { title, content } = req.body;
+  const newPost = {
+    id: Date.now(),
+    title,
+    content
+  };
+  posts.push(newPost);
+  console.log(posts);
+  res.redirect("/");
+})
 
 app.listen(port, () => {
   console.log(`This app is running on port ${port}`);
