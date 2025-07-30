467fd41dea4b4bf9518f66ef8a1e84a2aef391b5
Akradeth Tayarat <a.tayarat@gmx.net>
2025-07-30 21:43:44 +0200

    Commit 30.07.2025

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
 
-const posts = [];
 const app = express();
 const port = 3000;
 
@@ -12,18 +11,39 @@ app.get("/", (req, res) => {
   res.render("index.ejs", { posts });
 });
 
-app.post("/posts", (req, res)=>{
+app.get("/edit", (req, res) => {
+  res.render("edit.ejs", { posts });
+});
+
+app.post("/posts", (req, res) => {
   const { title, content } = req.body;
   const newPost = {
     id: Date.now(),
     title,
-    content
+    content,
   };
   posts.push(newPost);
-  console.log(posts);
-  res.redirect("/");
-})
+  res.render("index.ejs", { posts });
+});
 
 app.listen(port, () => {
   console.log(`This app is running on port ${port}`);
 });
+
+const posts = [
+  {
+    id: 1,
+    title: "My first blogpost",
+    content: "This is the content for my first blogpost",
+  },
+  {
+    id: 2,
+    title: "My second blogpost",
+    content: "This is the content for my second blogpost",
+  },
+  {
+    id: 3,
+    title: "My second blogpost",
+    content: "This is the content for my second blogpost",
+  },
+];
